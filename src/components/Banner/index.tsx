edfbfc3bba38e4c11fb731e7a3228d52e69289fb
Etcloide <etcloide@gmail.com>
2024-07-05 00:08:43 -0300

    details

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,9 +1,12 @@
 import styles from "./Banner.module.scss";
+
 const Banner = () => {
   return (
     <>
       <div className={styles.banner}>
-        <h1 className={styles.text}>Seja Bem-Vindo(a) à essa cafeteria!</h1>
+        <div className={styles.mask}>
+        </div>
+          <h1 className={styles.text}>Seja Bem-Vindo(a) à essa cafeteria!</h1>
       </div>
     </>
   );
